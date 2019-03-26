ff406fcf32c309a8105487cee1c84ceae558e21f
hlufeng <applesobig@sina.com>
2019-03-26 23:49:17 +0800

    摄氏度转华氏度

--- app/src/main/java/com/example/lenovo/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/lenovo/myapplication/Main2Activity.java
@@ -27,7 +27,7 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
 //        摄氏温标（C）和华氏温标（F）之间的换算关系为：
 //        F=C×1.8+32;
         String f1 =String.valueOf(f);
-        //1代表小数点后面的位数, 不足补0。f代表数据是浮点类型。保留2位小数就是“%.2f”。
+        //1代表小数点后面的位数, 不足补0。f代表数据是浮点类型。保留2位小数就是“%.2f”
         out.setText(f1+"°F");
     }
 }
